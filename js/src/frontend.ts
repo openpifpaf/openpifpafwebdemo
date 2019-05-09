a838a8bfd9237062a2c771ea49ffd44ca091f759
Sven Kreiss <me@svenkreiss.com>
2019-05-09 13:50:40 +0200

    tweaks

--- js/src/frontend.ts
+++ js/src/frontend.ts
@@ -19,25 +19,35 @@ let lastProcessing: number = null;
 const c = new Camera(document.getElementById('capture'));
 const vis = new Visualization(document.getElementById('visualization'));
 
-export function newImage() {
+export async function newImage() {
     const data = c.imageData();
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', backend_location + '/process', true);
-    xhr.onload = function() {
-        if (lastProcessing != null) {
-            const duration = Date.now() - lastProcessing;
-            fps = 0.5 * fps + 0.5 * (1000.0 / duration);
-            fpsSpan.textContent = `${fps.toFixed(1)}`;
-        }
-        lastProcessing = Date.now();
-        newImage();
-        const body = JSON.parse(this['responseText']);
-        vis.draw(data.image, body);
-    };
-    xhr.onerror = () => setTimeout(newImage, 1000);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
+    await new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', backend_location + '/process', true);
+        xhr.onload = async function() {
+            if (lastProcessing != null) {
+                const duration = Date.now() - lastProcessing;
+                fps = 0.5 * fps + 0.5 * (1000.0 / duration);
+                fpsSpan.textContent = `${fps.toFixed(1)}`;
+            }
+            lastProcessing = Date.now();
+
+            const body = JSON.parse(this['responseText']);
+            await vis.draw(data.image, body);
+            resolve();
+        };
+        xhr.onerror = () => reject();
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    });
 }
 
-newImage();  // kick it off
+
+async function loop_forever() {
+    while (true) {
+        await newImage();
+        await new Promise(resolve => requestAnimationFrame(() => resolve()));
+    }
+}
+loop_forever();
