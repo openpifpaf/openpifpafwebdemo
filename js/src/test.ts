956e018ec78b5bc83f4ab025cda90a072f1255f6
Sven Kreiss <me@svenkreiss.com>
2020-06-25 20:46:11 +0200

    fix tests. Drop Python 3.5

--- js/src/test.ts
+++ js/src/test.ts
@@ -12,7 +12,6 @@ describe('Server Process', () => {
   envVars.PORT = '5002';
   const server_process = child_process.spawn('python', [
     '-m', 'openpifpafwebdemo.server',
-    '--checkpoint', 'shufflenetv2x1',
   ], {
       env: envVars,
   });
